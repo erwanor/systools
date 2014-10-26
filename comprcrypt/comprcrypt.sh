4c63bb97d28d92c4ac3f7722ade89e81d05ffded
aaronwinter <eatounno@uwaterloo.ca>
2014-10-26 16:06:56 +0000

    fixed some edege cases + code structure

--- comprcrypt/comprcrypt.sh
+++ comprcrypt/comprcrypt.sh
@@ -5,3 +5,30 @@
 #        2014        #
 #    MIT LICENSE     #
 ######################
+
+# Your GPG ID
+GPG=59D02DF1
+target=${2%/}
+
+if [ "$1" == "-c" ] ; then
+  tar -cjf $target.tar.bz2 $target && \
+  gpg -r $GPG --encrypt $target.tar.bz2 && \
+  rm $target.tar.bz2 && \
+  rm -r $target && \
+  mv $target.tar.bz2.gpg ${target%.*}.gsa
+  echo "success: $target.gsa is an encrypted archive"
+  exit 0
+fi
+
+if [ "$1" == "-e" ] ; then
+  gpg --output ${target%.*} --decrypt $target && \
+  tar -xjf ${target%.*} && \
+  rm -v $target
+  echo "success: ${target%.*} is a clear directory"
+  exit 0
+fi
+
+echo "comprcrypt -ce /path/to/target"
+echo "     -c : compress"
+echo "     -e : extract"
+exit 1
