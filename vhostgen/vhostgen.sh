2169da97d4556a2c6f8ba9daf4b6fa681910557c
aaronwinter <eatounno@uwaterloo.ca>
2014-10-26 14:36:44 +0000

    code style + Todo list

--- vhostgen/vhostgen.sh
+++ vhostgen/vhostgen.sh
@@ -33,16 +33,15 @@ function CheckDomain(){
   echo $1 > $config_dir/in.tmp
   input=$(grep -P '^[a-zA-Z0-9]*[.][a-zA-Z]*$' $config_dir/in.tmp)
   rm $config_dir/in.tmp
-  if [ -z "$input" ]
-  then
-    if [ "$debug" = "1" ]
-    then
+  if [ -z "$input" ]; then
+    if [ "$debug" = "1" ]; then
       echo "0"
+
     else
       echo "incorrect domain name"
     fi
-  elif [ "$input" = "$1" ]
-  then
+
+  elif [ "$input" = "$1" ]; then
     echo "1"
   fi
 }
@@ -50,16 +49,17 @@ function CheckDomain(){
 # CheckDirectory $string -> boolean
 # Check if $string is an existing directory
 function CheckDirectory(){
-  if [ -d $1 ]
-  then
+  if [ -d $1 ]; then
     echo "1"
+
   else
-    if [ "$debug" = "0" ]
-    then
+    if [ "$debug" = "0" ]; then
       echo "0"
+
     else
       echo "directory does not exist"
     fi
+
   fi
 }
 
@@ -75,15 +75,6 @@ function EscapeDirectory(){
 # Sanitize string -> string
 # Escape all non-alphanumerical characters
 
-# CheckAPI string -> boolean
-# Check validity of Namecheap API Key
-: 'function CheckAPI(){
-  api_command="namecheap.domains.getList"
-  call="https://api.namecheap.com/xml.response?ApiUser=$api_user&ApiKey=$api_key&UserName=$nc_user&ClientIp=$client_ip&Command=$api_command"
-  output= curl $call
-  echo $output
-}'
-
 # AddRecord string , string -> boolean
 # Add subdomain to host records
 function SetRecord(){
@@ -95,8 +86,10 @@ function SetRecord(){
 
   if grep -q $subdomain "$config_dir/hosts"; then if [ "$debug" = "1" ]; then echo "0"; else echo "subdomain exist already"; exit 0; fi fi
   numberOfRecords=$(cat "$config_dir/hosts" | wc -l)
+
   if [ $numberOfRecords -ge 10 ]; then if [ "$debug" = "1" ]; then echo "0"; else echo "There is too many subdomains"; exit 0; fi fi
   cursor=1
+
   while [ $cursor -le $numberOfRecords ]; do
     line=$cursor"p"
     path="$config_dir/hosts"
@@ -107,79 +100,53 @@ function SetRecord(){
   call+="&HostName$cursor=$subdomain&RecordType$cursor=A&Address$cursor=$target_ip"
   output= curl $call>$config_dir/tmp.xml
   success="OK"
-  if grep -q $success $config_dir/tmp.xml
-  then
+  if grep -q $success $config_dir/tmp.xml; then
     echo ""
     echo "$subdomain">>"$config_dir/hosts"
     echo "record successfully create: $subdomain.$domain_sld.$domain_tld is now available"
     rm $config_dir/tmp.xml
+
   else
     rm $config_dir/tmp.xml
     echo "0"
+
     if [ "$debug" = "1" ]; then
       echo $output>>"$config_dir/api_outputs.log"
       echo "API Call failed please contact admin"
     fi
+
   fi
 }
-
-# Xml parser
-# by chad on stackoverflow.com
-: 'function read_xml(){
-  local IFS=\>
-  read -d \< ENTITY CONTENT
-  local ret=$?
-  TAG_NAME=${ENTITY%% *}
-  ATTRIBUTES=${ENTITY#* }
-  return $ret
-}
-
-function parse_dom () {
-  if [[ $TAG_NAME = "foo" ]] ; then
-    eval local $ATTRIBUTES
-
-  echo "foo size is: $size"
-
-  elif [[ $TAG_NAME = "bar" ]] ; then
-    eval local $ATTRIBUTES
-    echo "bar type is: $type"
-  fi
-}
-
-function xml_wrapper(){
-  while read_dom; do
-        parse_dom
-    done
-}'
 
 # CheckExpect function input output
 # Test a function output
 function CheckExpect(){
-  if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] && [ "$1" != "CheckDirectory" ]
-  then
+  if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] && [ "$1" != "CheckDirectory" ]; then
     echo "usage: CheckExpect function inputToFunction outputOfFunction"
   fi
+
   output=$($1 $2)
-  if [ "$output" = "$3" ]
-  then
+  if [ "$output" = "$3" ]; then
     echo -ne "\e[1;32;40mSUCCESS:  "
     echo -e "\e[0;37;40m$1($2):\e[0m"
     echo -e "\e[1;33;40m SUPPOSED OUTPUT: $3\e[0m"
     echo -e "\e[1;33;40m OUTPUT: $output\e[0m"
-    return 1
+    if [ "$debug" = "1" ]; then echo "1"; fi
+
   else
     echo -ne "\e[1;31;40mFAILURE:  "
     echo -e "\e[0;37;40m$1($2):\e[0m"
     echo -e "\e[1;33;40m SUPPOSED OUTPUT: $3\e[0m"
     echo -e "\e[1;33;40m OUTPUT: $output\e[0m"
-    return 0
+
+    if [ "$debug" = "0" ]; then echo "0"; fi
+
   fi
 }
 
 SetRecord
 
-if [ "CheckDomain "$subdomain"" = "1" ] || [ "CheckDirectory "$serverpath"" = "1" ]
-then
+if [ "CheckDomain "$subdomain"" = "1" ] || [ "CheckDirectory "$serverpath"" = "1" ]; then
   echo "vhostgen usage: vhostgen domain.tld /path/to/public/directory"
   exit 0;
 else
@@ -192,8 +159,7 @@ else
 fi
 
 #@ Tests:
-if [ "$debug" = "1" ]
-then
+if [ "$debug" = "1" ]; then
   #@ CheckDomain
     CheckExpect CheckDomain "test.com" 1
     CheckExpect CheckDomain "1234567.com" "1"
